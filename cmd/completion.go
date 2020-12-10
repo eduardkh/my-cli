c412f6a9d9de6cd9190281fe61ca4c3dc04f17f5
Eduard Khiaev <eddietheone1984@gmail.com>
2020-12-10 18:57:37 +0200

    sdfsdf

--- cmd/completion.go
+++ cmd/completion.go
@@ -16,6 +16,7 @@ limitations under the License.
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -23,43 +24,19 @@ import (
 
 // completionCmd represents the completion command
 var completionCmd = &cobra.Command{
-	Use:   "completion [bash|zsh|fish|powershell]",
-	Short: "Generate completion script",
-	Long: `To load completions:
+	Use:   "completion",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
 
-Bash:
-
-$ source <(yourprogram completion bash)
-
-# To load completions for each session, execute once:
-Linux:
-  $ yourprogram completion bash > /etc/bash_completion.d/yourprogram
-MacOS:
-  $ yourprogram completion bash > /usr/local/etc/bash_completion.d/yourprogram
-
-Zsh:
-
-# If shell completion is not already enabled in your environment you will need
-# to enable it.  You can execute the following once:
-
-$ echo "autoload -U compinit; compinit" >> ~/.zshrc
-
-# To load completions for each session, execute once:
-$ yourprogram completion zsh > "${fpath[1]}/_yourprogram"
-
-# You will need to start a new shell for this setup to take effect.
-
-Fish:
-
-$ yourprogram completion fish | source
-
-# To load completions for each session, execute once:
-$ yourprogram completion fish > ~/.config/fish/completions/yourprogram.fish
-`,
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
 	DisableFlagsInUseLine: true,
 	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
 	Args:                  cobra.ExactValidArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("completion called")
 		switch args[0] {
 		case "bash":
 			cmd.Root().GenBashCompletion(os.Stdout)
@@ -71,4 +48,18 @@ $ yourprogram completion fish > ~/.config/fish/completions/yourprogram.fish
 			cmd.Root().GenPowerShellCompletion(os.Stdout)
 		}
 	},
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// completionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// completionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
