af4b8a4189ccbc2ef6d8c7e962b7a217dbc41394
Eduard Khiaev <eddietheone1984@gmail.com>
2020-12-16 20:00:13 +0200

    simple Unmarshal

--- testing/read write JSON/read/read.go
+++ testing/read write JSON/read/read.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 )
 
+// Session struct
+type Session struct {
+	Authenticated bool   `json:"authenticated"`
+	Token         string `json:"token"`
+}
+
 func main() {
-	content, err := ioutil.ReadFile("request.json")
+	// get json file content
+	jsonString, err := ioutil.ReadFile("request.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Print(string(content))
-
+	fmt.Print(string(jsonString))
+	// Session struct instance
+	var reading Session
+	// Unmarshal from byte slice to Session struct instance
+	err = json.Unmarshal([]byte(jsonString), &reading)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%+v\n", reading)
 }
