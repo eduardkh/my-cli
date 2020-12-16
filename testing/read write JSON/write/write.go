4d4e4c74b09655cb69f0b8051261e61a74290b29
Eduard Khiaev <eddietheone1984@gmail.com>
2020-12-16 13:21:44 +0200

    Marshal simple struck

--- testing/read write JSON/write/write.go
+++ testing/read write JSON/write/write.go
@@ -1,13 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
+// Book struck
 type Book struct {
-	Title  string
-	Author string
+	Title  string `json:"title"`
+	Author string `json:"author"`
 }
 
 func main() {
+	// struck instance
 	book := Book{Title: "Title", Author: "Author"}
+	fmt.Println("⬇️	Struct Version")
 	fmt.Printf("%+v\n", book)
+	// Marshal the instance to byte array
+	byteArray, err := json.Marshal(book)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("⬇️	JSON Version")
+	fmt.Println(string(byteArray))
 }
