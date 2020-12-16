df11517dbf5bceaa9522f14a04671348f7813689
Eduard Khiaev <eddietheone1984@gmail.com>
2020-12-16 13:30:08 +0200

    Marshal complex struct

--- testing/read write JSON/write/write.go
+++ testing/read write JSON/write/write.go
@@ -8,16 +8,25 @@ import (
 // Book struck
 type Book struct {
 	Title  string `json:"title"`
-	Author string `json:"author"`
+	Author Author `json:"author"`
+}
+
+// Author struck
+type Author struct {
+	Name      string `json:"name"`
+	Age       int    `json:"age"`
+	Developer bool   `json:"is_developer"`
 }
 
 func main() {
-	// struck instance
-	book := Book{Title: "Title", Author: "Author"}
+	// Author struck instance
+	author := Author{Name: "Name", Age: 36, Developer: true}
+	// Book struck instance
+	book := Book{Title: "Title", Author: author}
 	fmt.Println("⬇️	Struct Version")
 	fmt.Printf("%+v\n", book)
-	// Marshal the instance to byte array
-	byteArray, err := json.Marshal(book)
+	// Marshal the instance to byte array with indentation
+	byteArray, err := json.MarshalIndent(book, "", "    ")
 	if err != nil {
 		fmt.Println(err)
 	}
